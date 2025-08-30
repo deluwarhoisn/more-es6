e48d24c77fa777cec2f81d518504f9a78f6c30dd
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 21:22:57 +0600

    Add initial implementation of dot-bracet.js with student object and property access examples

---
+++ dot-bracet.js
@@ -0,0 +1,19 @@
+const student ={
+    name:`rafid`,
+    1:50,
+    marks:99
+
+}
+// dot notation
+
+const studentName = student.name;
+
+const studentOne = student['name']
+console.log(studentOne)
+
+for(const key in student){
+    const value = student[key]
+}
+
+const propName = 'marks'
+console.log(student[propName])
