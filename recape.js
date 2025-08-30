60daaf70a16d34e65340f2042172edaced4f946c
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 20:54:27 +0600

    Add initial implementation of recape.js with variable declarations, default parameters, template strings, and HTML structure

---
+++ recape.js
@@ -0,0 +1,27 @@
+// var let const
+const tax = 5000;
+let eat = 5;
+eat = 2
+
+const student = { name: `name`, mark: 50 }
+
+const friend = [`abul`, `babul`, `kabul`]
+
+//  default parameter
+
+function add(num1, num2 = 0) {
+
+}
+
+// template string
+const dynamicText = `my tax: ${tax} and marks ${student.mark * 1.2} has friend: ${friend[1]}
+
+}`
+
+const innerHTML = `
+<div>
+    <h1> hello: ${friend.length}</h1>
+</div>`
+
+
+//  arrow function
