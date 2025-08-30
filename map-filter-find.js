894424f21bd478472d34c6af09f4a68acb781aa2
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 23:05:31 +0600

    Add initial implementations of filter.js, find.js, forach.js, and map-filter-find.js with examples of array methods

---
+++ map-filter-find.js
@@ -0,0 +1,8 @@
+const students = [
+    {id:1,name:'abul', marks:50 },
+    {id:2,name:'nabul', marks:55 },
+    {id:3,name:'kabul', marks:95 },
+    {id:4,name:'dabul', marks:15 },
+]
+const names = students.map(student => student.name)
+console.log(names)
