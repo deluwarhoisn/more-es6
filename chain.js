d02d2f690920906843d33c1032edb7b03a2274cc
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 21:37:05 +0600

    Add initial implementation of chain.js with student object and optional chaining example

---
+++ chain.js
@@ -0,0 +1,18 @@
+const student ={
+    name:`rafid`,
+    1:50,
+    family:{
+        title:'bhuiah',
+        // mother:{
+        //     name:'nur jahan',
+        //     age: 45
+        // }
+    },
+    'home-address': 'kosu khet',
+    marks:99
+
+}
+
+// optional chine
+
+console.log(student.family.mother?.age)
