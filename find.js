3d724375b980b08d04c5bc3764df06ed95807c35
deluwarhoisn <babuhossen301@gmail.com>
2025-08-31 00:57:36 +0600

    find

--- find.js
+++ find.js
@@ -1,3 +1,5 @@
 const numbers = [ 1,5,16,10,45,2,7];
 const firstEven = numbers.find(x => x%2 === 0)
-console.log(firstEven)
+
+const greaterThan50 = numbers.find( num => num > 50)
+console.log(greaterThan50)
