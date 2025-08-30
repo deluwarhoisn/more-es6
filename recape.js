32787f4deed0eaf5f02d02b2db0f72f386a2bc04
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 21:05:09 +0600

    revision

--- recape.js
+++ recape.js
@@ -25,3 +25,15 @@ const innerHTML = `
 
 
 //  arrow function
+const add2 = (num1,num2 =0 ) => num1+ num2;
+const tenTimes = x => x * 10;
+
+//  spread 
+const newFriends = [...friends, `dabul`,`ebul`]
+
+
+// destructuring
+
+const {marks: totalMarks, age = 0 } = student 
+// console.log(totalMarks,age)
+const [firstFriend]= friends;
