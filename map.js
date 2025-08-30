126b0742f1ac3ba98280f6001193495bebb2f53e
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 22:17:32 +0600

    map

---
+++ map.js
@@ -0,0 +1,21 @@
+//  rulse 1
+
+// const numbers = [4,8,2,3,5];
+
+
+// const doubled = []
+// for(const num of numbers){
+//     const result = num*2;
+//     doubled.push(result);
+// }
+
+// console.log(doubled);
+
+// rulse-2
+
+const numbers = [4,8,2,3,5];
+// const doublelt = x => x * 2;
+// const doubled = numbers.map(doublelt) 
+
+const doubled = numbers.map(x => x * 2);
+console.log(doubled)
