89c9787090f8173136b49d949b1034d2cc4e30b8
deluwarhoisn <babuhossen301@gmail.com>
2025-08-30 23:40:10 +0600

    map-find-filter

---
+++ radeos.js
@@ -0,0 +1,8 @@
+const numbers = [4,5,6,7,12,45]
+
+// let sum =0;
+// for(const num of numbers){
+//     sum = sum + num
+// }
+const total = numbers.reduce((acc, curr) => acc + curr)
+console.log(total)
